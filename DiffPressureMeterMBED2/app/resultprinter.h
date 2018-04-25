5f06747ef33b0c324e8c2e25f5e59820414d78a2
Lukasz Uszko <lukasz.uszko@delphi.com>
2018-04-25 17:49:51 +0200

    ResultPrinter added

--- DiffPressureMeterMBED2/app/resultprinter.h
+++ DiffPressureMeterMBED2/app/resultprinter.h
@@ -2,15 +2,24 @@
 #define RESULTPRINTER_H
 
 #include "system.h"
-
+#include "AdafruitThermal.h"
+#include "externalrtc.h"
 
 class ResultPrinter
 {
 public:
-    ResultPrinter(PinName uartRX, PinNameuartTX);
+   typedef struct
+   {
+      Time rtcTime;
+   }ResultData;
+public:
+    ResultPrinter(PinName uartRxPin, PinName uartTxPin);
     bool printResult();
-
+    void setResultData(const ResultData &resultData);
+    static void runTest(AdafruitThermal& printer);
 private:
+    AdafruitThermal printer;
+    ResultData resultData;
 };
 
 #endif
